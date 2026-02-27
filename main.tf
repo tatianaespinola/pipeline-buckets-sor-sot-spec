0b2ad986ca7cb7067917be03e8c1da558a759e3c
Tatiana Espinola <espinola.data@gmail.com>
2026-02-26 21:47:51 -0300

    Fix S3 bucket naming conventions to use lowercase for investimentos

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "sor" {
-  bucket = "tatiana_bucket_SOR_investimentos"
+  bucket = "tatiana_bucket_sor_investimentos"
 }
 
 resource "aws_s3_bucket" "sot" {
-  bucket = "tatiana_bucket_SOT_investimentos"
+  bucket = "tatiana_bucket_sot_investimentos"
 }
 
 resource "aws_s3_bucket" "spec" {
-  bucket = "tatiana_bucket_SPEC_investimentos"
+  bucket = "tatiana_bucket_spec_investimentos"
 }
